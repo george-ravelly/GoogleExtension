b3987521a61ecdcbc7f81cb4586c6351aefebc05
george-ravelly <george.ravelly1@gmail.com>
2021-03-19 00:01:06 -0300

    melhorias no design da extensão

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let count = 0
+    let pokemon = {}
+    let pokemonName = ""
+    let imagem = ""
 
     document.querySelector("#btn").addEventListener("click", () => {
-        let text = document.querySelector("#nome")
+        let text = document.querySelector("#nome")    
         text.hidden = false
         text.value = ""
         let responder = document.querySelector("#enviar")
-        responder.hidden = false
+        responder.hidden = false  
         let nome = document.querySelector("#btn")
         nome.innerHTML = "Girar";
         let resultado = document.getElementById("resultado");
-        resultado.innerHTML = " ";
+        resultado.innerHTML = "";
         quiz()
     })
 
-    let count = 0
     document.querySelector("#enviar").addEventListener("click", () => {
         let nome = document.getElementById("nome");
         let resultado = document.getElementById("resultado");
         let acertos = document.getElementById("acertos");
-        if(nome.value === pokemonName){
-            document.getElementById('img').style = "-webkit-filter: grayscale(0%); filter: grayscale(0%); filter: gray;"
-            resultado.innerHTML ="Resposta certa!";
-            resultado.style = "color: #86FF33";
-            document.getElementById("enviar").hidden = true
-            acertos.innerHTML = ++count
+        if(nome.value !== ""){
+            if(nome.value === pokemonName){
+                document.getElementById('img').style = "-webkit-filter: grayscale(0%); filter: grayscale(0%); filter: gray;"
+                resultado.innerHTML ="Resposta certa!";
+                resultado.style = "color: #86FF33";
+                document.getElementById("enviar").hidden = true
+                acertos.hidden = false
+                acertos.innerHTML = ++count
+            }else{
+                document.getElementById("enviar").hidden = true
+                nome.value = " "
+                resultado.innerHTML = "Resposta errada! O nome correto é: "+pokemonName;
+                resultado.style = "color: #FF5733";
+            }
         }else{
-            document.getElementById("enviar").hidden = true
-            nome.value = " "
-            resultado.innerHTML = "Resposta errada!\n O nome correto é: "+pokemonName;
+            resultado.innerHTML = "Digite o nome do pokemon!!!";
             resultado.style = "color:#FF5733";
         }
+        
+        
     })
-
-    let pokemon = {}
-    let pokemonName = ""
-    let imagem = ""
+    
     function getPokemon(){
-        let random = (Math.random() * 810).toFixed(0)
+        let random = (Math.random() * 151).toFixed(0)
         //console.log(random)
         fetch("https://pokeapi.co/api/v2/pokemon/"+random)
             .then(response => response.json())
